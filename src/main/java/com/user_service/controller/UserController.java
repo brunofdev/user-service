3eefcc897b2b158229f1021855e6804f195cde87
Bruno de Fraga <brunofragaa97@gmail.com>
2025-09-27 00:26:41 -0300

    ajustado novamente cors

--- src/main/java/com/user_service/controller/UserController.java
+++ src/main/java/com/user_service/controller/UserController.java
@@ -10,7 +10,7 @@ import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "https://www.brunofragadev.com")
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
